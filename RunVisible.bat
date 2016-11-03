1b413cd4526ae9ae5257db24b4ab44c52bd34a2d
Michael Hex <texhex@users.noreply.github.com>
2016-11-03 18:58:33 +0100

    Fixed bug with sysnative (issue #5)

--- RunVisible.bat
+++ RunVisible.bat
@@ -1,24 +1,18 @@
-@echo off
+REM @echo off
 
 REM This can be run from MDT/SCCM like this (given you stored it in \Scripts\BiosSledgehammer):
 REM ---
 REM cmd.exe /c "%SCRIPTROOT%\BiosSledgehammer\RunVisible.bat"
 REM ---
 
+SET PS_PART_PATH=WindowsPowerShell\v1.0\powershell.exe
+
+SET PS_EXE=C:\Windows\System32\%PS_PART_PATH%
+SET PS_EXE_SYSNATIVE=c:\windows\sysnative\%PS_PART_PATH%
+
 REM We need to make sure to start the 64-bit PowerShell on a 64-bit machine.
-REM To avoid WoW we will need to use C:\Windows\sysnative
-IF "%PROGRAMFILES(X86)%"=="" GOTO X86
-GOTO X64
+REM If we are running in WoW, C:\windows\sysnative is active, in any other case it is not
+IF EXIST "%PS_EXE_SYSNATIVE%" SET PS_EXE=%PS_EXE_SYSNATIVE%
 
-:X86
-SET PS_EXE=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
-GOTO RUN
-
-:X64
-SET PS_EXE=C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe
-GOTO RUN
-
-
-:RUN
 start "BiosSledgehammer" /wait %PS_EXE% -ExecutionPolicy Bypass -File "%~dp0BiosSledgehammer.ps1" -WaitAtEnd
 
